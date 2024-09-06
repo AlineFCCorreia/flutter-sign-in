14bb51380e62f77a2d012bc4d363a45f18a46c2c
Aline Correia <afcs.cin@gmail.com>
2024-09-06 11:06:56 -0300

    feat: modules added

---
+++ sign_in_afcs/lib/src/modules/user/profile/profile_module.dart
@@ -0,0 +1,17 @@
+
+import 'package:http/http.dart' as http;
+import 'package:flutter_modular/flutter_modular.dart';
+
+class ProfileModule extends Module{
+  void binds(Injector i){
+  
+    
+
+
+
+  }
+
+
+
+
+}
